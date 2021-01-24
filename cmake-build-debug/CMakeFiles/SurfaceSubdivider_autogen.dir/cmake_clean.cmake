09faad5b497b4564ec0ec4c4d1bbf612a0e06acb
Wajov <waj19971112@qq.com>
2021-01-25 00:05:00 +0800

    added gitignore

--- cmake-build-debug/CMakeFiles/SurfaceSubdivider_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SurfaceSubdivider_autogen"
-  "SurfaceSubdivider_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SurfaceSubdivider_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
